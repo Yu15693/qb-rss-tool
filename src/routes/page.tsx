f8363bd50a67384f66abe4748b8d16356328a3b8
yu <yugreyson18@gmail.com>
2024-04-30 00:33:47 +0800

    feat: add react-hook-form

--- src/routes/page.tsx
+++ src/routes/page.tsx
@@ -1,28 +1,59 @@
 import { Button, Box, TextField, IconButton } from '@mui/material';
-import { useState } from 'react';
 import { ClearOutlined as IconClearOutlined } from '@mui/icons-material';
+import { useForm, Controller } from 'react-hook-form';
+
+interface IForm {
+  rssUrl: string;
+}
 
 export default function Index() {
-  const [addressValue, setAddressValue] = useState('');
+  const { control, handleSubmit } = useForm<IForm>({
+    defaultValues: {
+      rssUrl: '',
+    },
+  });
+
+  const onSubmit = (data: IForm) => {
+    console.log(data);
+  };
 
   return (
     <Box>
-      <Box display="flex" gap={2}>
-        <TextField
-          label="输入RSS地址"
-          type="text"
-          fullWidth
-          value={addressValue}
-          onChange={e => setAddressValue(e.target.value)}
-          InputProps={{
-            endAdornment: addressValue ? (
-              <IconButton onClick={() => setAddressValue('')}>
-                <IconClearOutlined />
-              </IconButton>
-            ) : undefined,
-          }}
-        />
-        <Button variant="contained">新增</Button>
+      <Box>
+        <Box
+          component="form"
+          display="flex"
+          gap={2}
+          onSubmit={handleSubmit(onSubmit)}
+        >
+          <Controller
+            name="rssUrl"
+            control={control}
+            render={({ field }) => {
+              const { value, onChange } = field;
+              return (
+                <TextField
+                  label="输入RSS地址"
+                  type="text"
+                  fullWidth
+                  {...field}
+                  InputProps={{
+                    endAdornment: value ? (
+                      <IconButton
+                        onClick={() => onChange({ target: { value: '' } })}
+                      >
+                        <IconClearOutlined />
+                      </IconButton>
+                    ) : undefined,
+                  }}
+                />
+              );
+            }}
+          />
+          <Button variant="contained" type="submit">
+            新增
+          </Button>
+        </Box>
       </Box>
     </Box>
   );
