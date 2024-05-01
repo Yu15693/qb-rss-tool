3848c26c296b72f485258d32db0502c1b8369ff3
yu <yugreyson18@gmail.com>
2024-05-01 15:33:06 +0800

    feat: sync subItem edit value

--- src/views/index/SubItemBlock/index.tsx
+++ src/views/index/SubItemBlock/index.tsx
@@ -1,5 +1,84 @@
-import { Box } from '@mui/material';
+import { Box, ListSubheader, Stack, TextField } from '@mui/material';
+import { useEffect, useState } from 'react';
+import { useDebounceFn } from 'ahooks';
+import { useIndexStore } from '../store';
 
 export default function SubItemBlock() {
-  return <Box>SubItemBlock</Box>;
+  const indexStore = useIndexStore();
+  const selectedSubItem = indexStore.findSubItem(indexStore.selectedLink);
+  const [tempValue, setTempValue] = useState({
+    title: '',
+    mustContain: '',
+    mustNotContain: '',
+  });
+
+  const { run: syncTempValue } = useDebounceFn(
+    (link: string, value: typeof tempValue) => {
+      indexStore.editSubItem(link, value);
+    },
+    { wait: 100 },
+  );
+
+  const changeTempValue = (value: Partial<typeof tempValue>) => {
+    setTempValue(v => {
+      return {
+        ...v,
+        ...value,
+      };
+    });
+    if (selectedSubItem) {
+      syncTempValue(selectedSubItem.link, value);
+    }
+  };
+
+  useEffect(() => {
+    if (selectedSubItem) {
+      setTempValue({
+        title: selectedSubItem.title,
+        mustContain: selectedSubItem.mustContain,
+        mustNotContain: selectedSubItem.mustNotContain,
+      });
+    }
+  }, [selectedSubItem]);
+  return (
+    <Box height="100%">
+      <ListSubheader>配置详情</ListSubheader>
+      {selectedSubItem && (
+        <>
+          <Box paddingLeft={2}>
+            <TextField
+              label="标题"
+              type="text"
+              inputProps={{ maxLength: 100 }}
+              fullWidth
+              value={tempValue.title}
+              onChange={e => changeTempValue({ title: e.target.value })}
+              sx={{ marginBottom: 2 }}
+            />
+            <Stack direction="row" gap={2}>
+              <TextField
+                label="必须包含"
+                type="text"
+                inputProps={{ maxLength: 100 }}
+                fullWidth
+                value={tempValue.mustContain}
+                onChange={e => changeTempValue({ mustContain: e.target.value })}
+              />
+              <TextField
+                label="必须不含"
+                type="text"
+                inputProps={{ maxLength: 100 }}
+                fullWidth
+                value={tempValue.mustNotContain}
+                onChange={e =>
+                  changeTempValue({ mustNotContain: e.target.value })
+                }
+              />
+            </Stack>
+          </Box>
+          <ListSubheader>匹配项</ListSubheader>
+        </>
+      )}
+    </Box>
+  );
 }
--- src/views/index/store.ts
+++ src/views/index/store.ts
@@ -15,6 +15,7 @@ interface IndexState {
   selectedLink: string;
   setLink: (link: string) => void;
   addSubItem: (subItem: SubItem) => void;
+  editSubItem: (link: string, subItem: Partial<SubItem>) => void;
   removeSubItem: (link: string) => void;
   clearSubList: () => void;
   findSubItem: (link: string) => SubItem | undefined;
@@ -36,6 +37,20 @@ export const useIndexStore = create<IndexState>()(
         addSubItem(subItem: SubItem) {
           const newSubList = [...get().subList, subItem];
           set({ subList: newSubList });
+        },
+        editSubItem(link, subItem) {
+          const newSubList = get().subList.map(v => {
+            if (v.link !== link) {
+              return v;
+            }
+            return {
+              ...v,
+              ...subItem,
+            };
+          });
+          set({
+            subList: newSubList,
+          });
         },
         removeSubItem(link) {
           const newSubList = get().subList.filter(v => v.link !== link);
